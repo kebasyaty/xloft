c076067bf3033ecb477487369151c66e0920bd06
kebasyaty <kebasyaty@gmail.com>
2025-08-17 21:08:40 +0300

    add a `QuantumLoop` class

--- src/xloft/quantum.py
+++ src/xloft/quantum.py
@@ -1,18 +1,83 @@
-"""A set of tools for quantum calculations.
+"""A set of tools for quantum calculations."""
 
-A qubit in a regular computer is some algorithm that is executed in
-one iteration of a cycle in a separate processor thread.
-
-The quantum state of such a qubit is the potential result of the executed algorithm.
-
-The result of the qubit operation can be `True`, `False`, `None`, `int`, `float`, `str`, `Exception`, etc.
-
-How to use the qubit result is up to you to decide based on your goals.
-"""
-
+import concurrent.futures
 import multiprocessing
+from enum import Enum
+from typing import Any, Callable, Iterable, Never, assert_never
 
 
 def count_qubits() -> int:
-    """Counting the number of qubits of your computer."""
+    """Counting the number of qubits of your computer.
+
+    A qubit in a regular computer is some algorithm that is executed in
+    one iteration of a cycle in a separate processor thread.
+
+    Returns:
+        The number of qubits.
+    """
     return multiprocessing.cpu_count()
+
+
+class LoopMode(Enum):
+    """Quantum loop mode."""
+
+    PROCESS_POOL = 1
+    THREAD_POOL = 2
+
+
+class QuantumLoop:
+    """Divide the cycle into quantums."""
+
+    def __init__(  # noqa: D107
+        self,
+        quantum: Callable,
+        data: Iterable[Any],
+        timeout: float | None = None,
+        chunksize: int = 1,
+        mode: LoopMode = LoopMode.PROCESS_POOL,
+    ) -> None:
+        self.quantum = quantum
+        self.data = data
+        self.timeout = timeout
+        self.chunksize = chunksize
+        self.mode = mode
+
+    def process_pool(self) -> list[Any]:
+        """Better suitable for operations for which large processor resources are required."""
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            results = list(
+                executor.map(
+                    self.quantum,
+                    self.data,
+                    timeout=self.timeout,
+                    chunksize=self.chunksize,
+                ),
+            )
+        return results
+
+    def thread_pool(self) -> list[Any]:
+        """More suitable for tasks related to input-output
+        (for example, network queries, file operations),
+        where GIL is freed during input-output operations."""  # noqa: D205, D209
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = list(
+                executor.map(
+                    self.quantum,
+                    self.data,
+                    timeout=self.timeout,
+                    chunksize=self.chunksize,
+                ),
+            )
+        return results
+
+    def run(self) -> list[Any]:
+        """Run the quantum loop."""
+        results: list[Any] = []
+        match self.mode.value:
+            case 1:
+                results = self.process_pool()
+            case 2:
+                results = self.thread_pool()
+            case _ as unreachable:
+                assert_never(Never(unreachable))
+        return results
--- tests/test_quantum.py
+++ tests/test_quantum.py
@@ -2,9 +2,29 @@
 
 import multiprocessing
 
-from xloft.quantum import count_qubits
+from xloft.quantum import LoopMode, QuantumLoop, count_qubits
 
 
 def test_count_qubits() -> None:
     """Testing a count_qubits method."""
     assert count_qubits() == multiprocessing.cpu_count()
+
+
+class TestQuantumLoop:
+    """Testing a `QuantumLoop` class."""
+
+    def quantum(self, item):
+        """Test task."""
+        return item * item
+
+    def test_process_pool(self) -> None:
+        """Testing a `process_pool` method."""
+        data = range(10)
+        qloop = QuantumLoop(self.quantum, data, mode=LoopMode.PROCESS_POOL)
+        assert qloop.run() == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+    def test_thread_pool(self) -> None:
+        """Testing a `thread_pool` method."""
+        data = range(10)
+        qloop = QuantumLoop(self.quantum, data, mode=LoopMode.THREAD_POOL)
+        assert qloop.run() == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
