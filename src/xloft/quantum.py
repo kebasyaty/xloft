322d941576706f58523d232ef014e8016f828af9
kebasyaty <kebasyaty@gmail.com>
2025-08-18 08:26:44 +0300

    fix docstring to QuantumLoop

--- src/xloft/quantum.py
+++ src/xloft/quantum.py
@@ -43,15 +43,15 @@ class QuantumLoop:
     """Separation of the cycle into quantums.
 
     Args:
-    quantum: A function that describes the task.
-    data: The data that needs to be processed.
-    timeout: The maximum number of seconds to wait. If None, then there
-    is no limit on the wait time.
-    chunksize: The size of the chunks the iterable will be broken into
-    before being passed to a child process. This argument is only
-    used by ProcessPoolExecutor; it is ignored by
-    ThreadPoolExecutor.
-    mode: The operating mode for a quantum loop: LoopMode.PROCESS_POOL | LoopMode.THREAD_POOL.
+        quantum: A function that describes the task.
+        data: The data that needs to be processed.
+        timeout: The maximum number of seconds to wait. If None, then there
+        is no limit on the wait time.
+        chunksize: The size of the chunks the iterable will be broken into
+        before being passed to a child process. This argument is only
+        used by ProcessPoolExecutor; it is ignored by
+        ThreadPoolExecutor.
+        mode: The operating mode for a quantum loop: LoopMode.PROCESS_POOL | LoopMode.THREAD_POOL.
 
     Examples:
         >>> from xloft.quantum import LoopMode, QuantumLoop, count_qubits
