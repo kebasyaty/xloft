5113b630a46c4b62344715f2a5c62af72372bb79
kebasyaty <kebasyaty@gmail.com>
2025-12-12 12:45:20 +0200

    add a brand comment

--- src/xloft/__init__.py
+++ src/xloft/__init__.py
@@ -1,3 +1,15 @@
+#
+#
+# `YMM'   `MP' `7MMF'        .g8""8q. `7MM"""YMM MMP""MM""YMM
+#   VMb.  ,P     MM        .dP'    `YM. MM    `7 P'   MM   `7
+#    `MM.M'      MM        dM'      `MM MM   d        MM
+#      MMb       MM        MM        MM MM""MM        MM
+#    ,M'`Mb.     MM      , MM.      ,MP MM   Y        MM
+#   ,P   `MM.    MM     ,M `Mb.    ,dP' MM            MM
+# .MM:.  .:MMa..JMMmmmmMMM   `"bmmd"' .JMML.        .JMML.
+#
+#
+
 """(XLOFT) X-Library of tools.
 
 Modules exported by this package:
