794f5b5bf3dc6736829842e086cd1e0a818be0df
kebasyaty <kebasyaty@gmail.com>
2026-01-14 14:32:09 +0200

    add a run_action method to AliasDict

--- src/xloft/types/alias_dict.py
+++ src/xloft/types/alias_dict.py
@@ -7,11 +7,45 @@ from __future__ import annotations
 
 __all__ = ("AliasDict",)
 
-from typing import Any
+from typing import Any, Never, assert_never
 
 
-class AliasDict:  # noqa: B903
+class AliasDict:
     """Pseudo dictionary with supports aliases for keys."""
 
-    def __init__(self, data: list[tuple[list[str | int | float], Any]]) -> None:  # noqa: D107
-        self.store = data
+    def __init__(self, data: list[tuple[set[str | int | float], Any]]) -> None:  # noqa: D107
+        self.store = []
+        for item in data:
+            self.store.append(list(item))
+
+    def run_action(
+        self,
+        alias: str | int | float,
+        value: Any | None = None,
+        action: str | None = None,
+    ) -> bool:
+        """Perform an action on a dictionary."""
+        is_alias_present: bool = False
+
+        for item in self.store:
+            if alias in item[0]:
+                is_alias_present = True
+                match action:
+                    case "get":
+                        pass
+                    case "set":
+                        item[1] = value
+                    case "delete":
+                        pass
+                    case "add_alias":
+                        pass
+                    case "delete_alias":
+                        pass
+                    case "has_alias":
+                        pass
+                    case "has_value":
+                        pass
+                    case _ as unreachable:
+                        assert_never(Never(unreachable))  # pyrefly: ignore[not-callable]
+
+        return is_alias_present
