fa4a3d4f35e1dd10656e0b0d95b73afecdae7f54
kebasyaty <kebasyaty@gmail.com>
2026-01-14 08:10:10 +0200

    update alias_dict

--- src/xloft/types/alias_dict.py
+++ src/xloft/types/alias_dict.py
@@ -1,12 +1,17 @@
 # XLOFT - X-Library of tools.
 # Copyright (c) 2025 Gennady Kostyunin
 # SPDX-License-Identifier: MIT
-"""???"""
+"""AliasDict - Simulates the behavior of a dictionary and supports aliases for keys."""
 
 from __future__ import annotations
 
 __all__ = ("AliasDict",)
 
+from typing import Any
+
 
 class AliasDict:
-    """???"""
+    """Dictionary with supports aliases for keys."""
+
+    def __init__(self, **kwargs: dict[list[str | int | float], Any]) -> None:  # noqa: D107
+        pass
