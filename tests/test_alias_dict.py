1a15b8a1cfe975a1e9a49dc37f1a6b549503bbac
kebasyaty <kebasyaty@gmail.com>
2026-01-14 08:00:15 +0200

    add test_alias_dict

---
+++ tests/test_alias_dict.py
@@ -0,0 +1,19 @@
+"""Testing a `AliasDict` module."""
+
+from __future__ import annotations
+
+
+class TestNegative:
+    """Negative tests."""
+
+    def test_stub(self) -> None:
+        """Testing stub."""
+        assert True
+
+
+class TestPositive:
+    """Positive tests."""
+
+    def test_stub(self) -> None:
+        """Testing stub."""
+        assert True
