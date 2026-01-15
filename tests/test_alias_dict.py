dc1b13ca4864e19b38e0324d497d13ed70f7e640
kebasyaty <kebasyaty@gmail.com>
2026-01-15 13:02:08 +0200

    add test_get_key_and_value

--- tests/test_alias_dict.py
+++ tests/test_alias_dict.py
@@ -25,15 +25,33 @@ class TestPositive:
         """Test add value to empty dictionary."""
         d = AliasDict()
 
-        d.set("alias name", "Hello world!")
+        d.add("alias name", "Hello world!")
         assert d.get("alias name") == "Hello world!"
 
-        d.set(5, 5)
+        d.add(5, 5)
         assert d.get(5) == 5
 
-        d.set(5.1, 5.1)
+        d.add(5.1, 5.1)
         assert d.get(5.1) == 5.1
 
         assert "alias name" in d.all_alias_set
         assert 5 in d.all_alias_set
         assert 5.1 in d.all_alias_set
+
+    def test_get_key_and_value(self) -> None:
+        """Test get value from dictionary."""
+        data = [
+            ({"English", "en"}, "lemmatize_en_all"),
+            ({"Russian", "ru"}, "lemmatize_ru_all"),
+            ({"German", "de"}, "lemmatize_de_all"),
+        ]
+        d = AliasDict(data)
+
+        assert d.get("English") == "lemmatize_en_all"
+        assert d.get("en") == "lemmatize_en_all"
+
+        assert d.get("Russian") == "lemmatize_ru_all"
+        assert d.get("ru") == "lemmatize_ru_all"
+
+        assert d.get("German") == "lemmatize_de_all"
+        assert d.get("de") == "lemmatize_de_all"
