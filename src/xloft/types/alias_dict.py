11b99af797a00f5b85873f6258b3b82866a74da6
kebasyaty <kebasyaty@gmail.com>
2026-01-19 17:57:19 +0200

    update docstring to alias_dict.py

--- src/xloft/types/alias_dict.py
+++ src/xloft/types/alias_dict.py
@@ -13,9 +13,9 @@ Class `AliasDict` contains the following methods:
 - `delete_alias` - Remove the alias from the existing set.
 - `has_key` - Check if the alias exists.
 - `has_value` - Check if the value exists.
-- `items` - Returns a list of `AliasDict` elements grouped into tuples.
-- `keys` - Get a list of all aliases.
-- `values` - Get a list of all values.
+- `items` - Returns a generator of list of `AliasDict` elements grouped into tuples.
+- `keys` - Get a generator of list of all aliases.
+- `values` - Get a generator of list of all values.
 """
 
 from __future__ import annotations
